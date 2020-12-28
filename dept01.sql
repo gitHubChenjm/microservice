cc1b4cbe004270cdbe1b5d7a248dc980c449d3f1
chenjm <chenjiaman000@qq.com>
2020-12-28 10:34:19 +0800

    增加数据库文件

---
+++ dept01.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80022
+ Source Host           : localhost:3306
+ Source Schema         : dept01
+
+ Target Server Type    : MySQL
+ Target Server Version : 80022
+ File Encoding         : 65001
+
+ Date: 28/12/2020 10:33:03
+*/
+
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` int(0) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '研发部', 'dept01');
+INSERT INTO `dept` VALUES (2, '产品部', 'dept01');
+INSERT INTO `dept` VALUES (3, '实施部', 'dept01');
+INSERT INTO `dept` VALUES (4, '总裁部', 'dept01');
+
