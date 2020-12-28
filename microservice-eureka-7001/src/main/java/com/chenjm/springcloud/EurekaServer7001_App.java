98378e15e208cd419c5254a0473bcf325ace7eea
chenjm <chenjiaman000@qq.com>
2020-12-28 11:29:19 +0800

    阶段二结束：最终效果服务提供者项目（8001），成功注册到eureka服务上面

--- microservice-eureka-7001/src/main/java/com/chenjm/springcloud/EurekaServer7001_App.java
+++ microservice-eureka-7001/src/main/java/com/chenjm/springcloud/EurekaServer7001_App.java
@@ -1,21 +1,20 @@
 package com.chenjm.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author chenjm
  * @version 1.0
  * @Description
  * @since 1.0
- * 2020/12/25
+ * 2020/12/28
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider8001_App {
+@EnableEurekaServer
+public class EurekaServer7001_App {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class);
+        SpringApplication.run(EurekaServer7001_App.class);
     }
 }
